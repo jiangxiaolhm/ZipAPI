dbd265753df13e52e59ef7ee353b92660b6fa944
Eric Lin <jiangxiao.lhm@gmail.com>
2019-10-27 05:51:41 +1100

    Add foreign key annotation

--- ZipAPI/Models/Account.cs
+++ ZipAPI/Models/Account.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace ZipAPI.Models
     {
         public int Id { get; set; }
         [Required]
+        [ForeignKey("UserId")]
         public User User { get; set; }
     }
 }
