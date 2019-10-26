21798a5d52032916f9d0a077da3764f218135fb3
Eric Lin <jiangxiao.lhm@gmail.com>
2019-10-27 04:51:13 +1100

    Add dbcontext

---
+++ ZipAPI/Data/ZipDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZipAPI.Models;
+
+namespace ZipAPI.Data
+{
+    public class ZipDbContext : DbContext
+    {
+        public ZipDbContext(DbContextOptions<ZipDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+    }
+}
