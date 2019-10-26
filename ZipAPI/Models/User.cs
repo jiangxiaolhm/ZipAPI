8469682cc44ecc34cc1f540805d859476d362fcd
Eric Lin <jiangxiao.lhm@gmail.com>
2019-10-27 04:50:48 +1100

    Add models

---
+++ ZipAPI/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZipAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
+        [Column(TypeName = "Money")]
+        public decimal MonthlySalary { get; set; }
+        [Required]
+        [Column(TypeName = "Money")]
+        public decimal MonthlyExpenses { get; set; }
+        public Account Account { get; set; }
+    }
+}
